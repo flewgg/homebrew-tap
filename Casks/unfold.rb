b537cd75ae81fbd36dcdd5f629c1d8faebea6dfa
Jacob <nerd@isolu.me>
2026-03-26 02:46:24 -0500

    update unfold tap to v2.0.0

--- Casks/unfold.rb
+++ Casks/unfold.rb
@@ -1,12 +1,12 @@
 cask "unfold" do
-  version "1.2.1"
-  sha256 "b3cafe22e7be16d078470bf6be285e738aa413530f0cbc015a630c8cbbd35813" 
+  version "2.0.0"
+  sha256 "de2c26f3448508c48f5fe4cb1c115588c425fc91a28a988f2fb27f6bbba7ae17"
 
   url "https://flew.gg/unfold/Unfold.dmg",
     verified: "flew.gg"
   name "Unfold"
   desc "A utility app to quickview folders, archives, code, and more."
-  homepage "https://flew.gg/projects/unfold"
+  homepage "https://flew.gg/unfold"
 
   auto_updates true
 
