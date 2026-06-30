adfc54ea1832faf2b80af9621272c618fb2206a0
Jacob <nerd@isolu.me>
2026-06-30 00:10:06 -0500

    Unfold v2.5.0

--- Casks/unfold.rb
+++ Casks/unfold.rb
@@ -1,6 +1,6 @@
 cask "unfold" do
-  version "2.4.0"
-  sha256 "4797c80cba7050abf71bb461bfc34065c00e0ec599d26f30c8fe4b69a0902514"
+  version "2.5.0"
+  sha256 "05f8ca6a7edc3f270b1af6d59fbea7492e942df347f601124e985c08b132b787"
 
   url "https://flew.gg/unfold/Unfold.dmg",
     verified: "flew.gg"
